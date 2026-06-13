22e09e74e9d29508dc117150f7488c12f731dc24
Germán Echeverri <ger.echeverri.la@gmail.com>
2026-06-12 22:14:29 -0400

    ventana de login creada

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import '../theme/app_colors.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.azul,
+      body: const Center(
+        child: Text(
+          'ORION',
+          style: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const azul = Color(0xFF004B85);
+  static const rojo = Color(0xFFD60019);
+  static const verde = Color(0xFF008452);
+  static const amarillo = Color(0xFFF7AE00);
+  static const negro = Color(0xFF000000);
+}

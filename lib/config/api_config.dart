03f9ef763fabe52743fdb51b72d2df1088fc845b
Germán Echeverri <ger.echeverri.la@gmail.com>
2026-06-15 15:45:02 -0400

    espacio para apis creado

---
+++ lib/config/api_config.dart
@@ -0,0 +1,8 @@
+class ApiConfig{
+    static const String baseUrl = 'http://localhost:3000';
+
+    static const String categoriasEndpoint = '/categorias';
+
+    static const String subcategoriasEndpoint = '/subcategorias';
+}
+//no hay back, asique no hay mucho aqui
